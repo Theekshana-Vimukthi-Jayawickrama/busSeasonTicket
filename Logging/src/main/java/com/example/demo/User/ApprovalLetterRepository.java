a67663e1642892c9ab5d4a6c830a6205ba40aa27
Amindi Garusinghe <amindigarusinghe2000@gmail.com>
2024-03-14 15:15:54 +0530

    aaa

--- Logging/src/main/java/com/example/demo/User/ApprovalLetterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface ApprovalLetterRepository extends JpaRepository<ApprovalLetter,Integer> {
-    Optional<ApprovalLetter> findById(Integer id);
-}
---
+++ bus_season_ticker_users/android/app/src/main/kotlin/com/example/bus_season_ticker_users/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bus_season_ticker_users
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
